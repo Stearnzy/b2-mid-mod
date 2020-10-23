cedcd1d73c90c2adfd9b0cb1ff18414638ff85b6
Zach Stearns <zach.stearns92@gmail.com>
2020-10-23 10:40:20 -0600

    add method to convert price to string

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,4 +1,8 @@
 class AmusementPark < ApplicationRecord
   validates_presence_of :name, :price_of_admission
   has_many :rides
+
+  def convert_price
+    "$#{self.price_of_admission}.00"
+  end
 end
