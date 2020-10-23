790c92059bc1e48308fea28e18daecdc36bd843d
Zach Stearns <zach.stearns92@gmail.com>
2020-10-23 10:18:59 -0600

    Write test for mechanic name and years of exp

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -7,5 +7,27 @@ describe "As a user" do
 
       expect(page).to have_content("All Mechanics")
     end
+
+    it "I see a list of all mechanic's names and their years of experience" do
+      mechanic_1 = Mechanic.create!({
+        name: "Sam Mills",
+        years_of_experience: 10
+        })
+
+      mechanic_2 = Mechanic.create!({
+        name: "Kara Smith",
+        years_of_experience: 11
+        })
+
+      visit "/mechanics"
+
+      within "#mechanic-#{mechanic_1}" do
+        expect(page).to have_content("#{mechanic_1.name} - #{mechanic_1.years_of_experience} years of experience")
+      end
+
+      within "#mechanic-#{mechanic_2}" do
+        expect(page).to have_content("#{mechanic_2.name} - #{mechanic_2.years_of_experience} years of experience")
+      end
+    end
   end
 end
