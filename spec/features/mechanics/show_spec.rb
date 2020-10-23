f0a84deedf0319ef471ab43821c782839dda9562
Zach Stearns <zach.stearns92@gmail.com>
2020-10-23 11:17:02 -0600

    Write test for mechanic show having name, years, and ride names

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+describe "As a user" do
+  describe "When I go to a mechanics show page" do
+    before(:each) do
+      @mechanic_1 = Mechanic.create!({
+        name: "Sam Mills",
+        years_of_experience: 10
+        })
+
+      @mechanic_2 = Mechanic.create!({
+        name: "Kara Smith",
+        years_of_experience: 11
+        })
+
+      @amusement_park_1 = AmusementPark.create!({
+        name: "Hershey Park",
+        price_of_admission: 50
+        })
+
+      @amusement_park_2 = AmusementPark.create!({
+        name: "Elitch's",
+        price_of_admission: 35
+        })
+
+      @ride_1 = Ride.create!({
+        name: "Lightning Racer",
+        thrill_rating: 9,
+        amusement_park_id: "#{@amusement_park_1.id}"
+        })
+
+      @ride_2 = Ride.create!({
+        name: "Storm Runner",
+        thrill_rating: 7,
+        amusement_park_id: "#{@amusement_park_1.id}"
+        })
+
+      @ride_3 = Ride.create!({
+        name: "Tower of Doom",
+        thrill_rating: 9,
+        amusement_park_id: "#{@amusement_park_2.id}"
+        })
+
+      @ride_mech_1 = RideMechanics.create!({
+        ride_id: @ride_1.id,
+        mechanic_id: @mechanic_1.id
+        })
+
+      @ride_mech_1 = RideMechanics.create!({
+        ride_id: @ride_3.id,
+        mechanic_id: @mechanic_1.id
+        })
+
+      @ride_mech_1 = RideMechanics.create!({
+        ride_id: @ride_2.id,
+        mechanic_id: @mechanic_3.id
+        })
+    end
+
+    it "I see their name, years of experience, and names of all the rides they're
+        working on" do
+      visit "/mechanics/#{@mechanic_1.id}"
+
+      expect(page).to have_content("Mechanic: #{@mechanic_1.name}")
+      expect(page).to have_content("Years of Experience: #{@mechanic_1.years_of_experience}")
+      expect(page).to have_content("Current rides they're working on:")
+      expect(page).to have_content("#{@ride_1.name}")
+      expect(page).to have_content("#{@ride_3.name}")
+    end
+  end
+end
