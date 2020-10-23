a08d4379c39ba060c0030a99f34ec0dc4703c8eb
Zach Stearns <zach.stearns92@gmail.com>
2020-10-23 11:05:42 -0600

    Add test for price conversion to string, average of thrill rides ratings

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -10,12 +10,36 @@ describe AmusementPark, type: :model do
     it { should validate_presence_of :price_of_admission }
   end
 
-  it "can convert price to string" do
-    amusement_park = AmusementPark.create!({
+  before(:each) do
+    @amusement_park = AmusementPark.create!({
       name: "Hershey Park",
       price_of_admission: 50
       })
 
-    expect(amusement_park.convert_price).to eq("$50.00")
+    @ride_1 = Ride.create!({
+      name: "Lightning Racer",
+      thrill_rating: 9,
+      amusement_park_id: "#{@amusement_park.id}"
+      })
+
+    @ride_2 = Ride.create!({
+      name: "Storm Runner",
+      thrill_rating: 7,
+      amusement_park_id: "#{@amusement_park.id}"
+      })
+
+    @ride_3 = Ride.create!({
+      name: "The Great Bear",
+      thrill_rating: 6,
+      amusement_park_id: "#{@amusement_park.id}"
+      })
+  end
+
+  it "can convert price to string" do
+    expect(@amusement_park.convert_price).to eq("$50.00")
+  end
+
+  it "can average the park's rides' thrill ratings" do
+    expect(@amusement_park.ride_rating_average.round(1)).to eq(7.3)
   end
 end
