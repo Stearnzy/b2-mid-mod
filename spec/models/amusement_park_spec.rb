6bb93ff7f4f3a972b275b77d89dedaa6fe46c8f5
Zach Stearns <zach.stearns92@gmail.com>
2020-10-23 09:35:00 -0600

    Add forgotten ends

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -8,4 +8,5 @@ describe AmusementPark, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :price }
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -9,4 +9,5 @@ describe Ride, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :thrill_rating }
+  end
 end
