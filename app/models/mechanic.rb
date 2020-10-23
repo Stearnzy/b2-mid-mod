4b9a2239aceae4318dc8608282665431e36077c8
Zach Stearns <zach.stearns92@gmail.com>
2020-10-23 11:53:19 -0600

    Add method for rides_ordered

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,4 +2,9 @@ class Mechanic < ApplicationRecord
   validates_presence_of :name, :years_of_experience
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+
+  def rides_ordered
+    rides.order(:name)
+  end
+
 end
